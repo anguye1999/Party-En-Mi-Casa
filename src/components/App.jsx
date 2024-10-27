1b1df0abcd1aaedcc61d8e26ed41df5de476cd9f
Ryan Tiffany <13257918+rtiffany@users.noreply.github.com>
2024-10-27 18:43:47 -0400

    fixed error with routing and updated npm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '../styles/App.css';
 import Home from './pages/Home.jsx';
 import WaitingRoom from './pages/WaitingRoom.jsx';
-import Join from './Pages/Join.jsx';
+import Join from './pages/Join.jsx';
 import Signup from './pages/Signup.jsx';
 import Login from './pages/Login.jsx';
 import ProtectedRoute from './ProtectedRoute.jsx';
